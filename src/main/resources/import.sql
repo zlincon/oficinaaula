3208827d7269163a20589aaf0e56f7dab999814d
Lincon <linconsilva08@gmail.com>
2020-09-26 13:04:35 -0300

    Configurando ambientes test e dev
    Confirgurando ambiente de test com h2 e ambiente de desenvolvedor com MySQL local.
    
    Incluindo auto-popular banco de dados h2

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO clientes (id, nome) VALUES (0, 'Roberto');
+INSERT INTO clientes (id, nome) VALUES (1, 'Luiza');
+INSERT INTO clientes (id, nome) VALUES (2, 'Adam');
+
+INSERT INTO veiculos (id, cor, marca, modelo, placa) VALUES (0, 'Blue', 'Alga', 'BC34', 'BCD4545');
+INSERT INTO veiculos (id, cor, marca, modelo, placa) VALUES (1, 'Green', 'WM', 'Sports Hibrid', 'GLK6896');
+INSERT INTO veiculos (id, cor, marca, modelo, placa) VALUES (2, 'Grey', 'Tesla', 'Jungle Tank', 'MIR7838');
