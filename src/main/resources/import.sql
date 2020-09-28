96363b715372021e15520e4cbc9dbb71bed7a157
Lincon Brito <38511541+zlincon@users.noreply.github.com>
2020-09-28 17:02:00 -0300

    Atualização db h2

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,3 +5,7 @@ INSERT INTO clientes (nome) VALUES ('Adam');
 INSERT INTO veiculos (cor, marca, modelo, placa, cliente_id) VALUES ('Blue', 'Alga', 'BC34', 'BCD4545', 1);
 INSERT INTO veiculos (cor, marca, modelo, placa, cliente_id) VALUES ('Green', 'WM', 'Sports Hibrid', 'GLK6896', 2);
 INSERT INTO veiculos (cor, marca, modelo, placa, cliente_id) VALUES ('Grey', 'Tesla', 'Jungle Tank', 'MIR7838', 3);
+
+INSERT INTO ordens_servico (descricao, preco, cliente_id, veiculo_id) VALUES ('Manutenção', 800, 1, 1)
+INSERT INTO ordens_servico (descricao, preco, cliente_id, veiculo_id) VALUES ('Manutenção', 800, 2, 2)
+INSERT INTO ordens_servico (descricao, preco, cliente_id, veiculo_id) VALUES ('Manutenção', 800, 3, 3)
